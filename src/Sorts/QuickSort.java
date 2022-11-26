29378f40ae2d635c55635a7e6a634cf3788e8f89
Varsha Chahal <varshachahal20@gmail.com>
2022-11-25 16:53:11 -0800

    quicksort approach 2 for pivot helper

--- src/Sorts/QuickSort.java
+++ src/Sorts/QuickSort.java
@@ -8,20 +8,19 @@ public class QuickSort{
         this.quickSortHelper(arr, 0, arr.length-1);
     }
 
-    public int[] quickSortHelper(int[] arr, int start, int end){
-        if(start>end){
-            System.out.println("ooooo");
+    public void quickSortHelper(int[] arr, int start, int end){
+        if(start>=end){
+           // System.out.println("ooooo");
+           return;
         }
-        if(start < end) {
-            int pivotIndex = pivotHelper(arr, start, end);
+            int pivotIndex = pivotHelper2(arr, start, end);
             quickSortHelper(arr, start,pivotIndex-1);
-            quickSortHelper(arr, pivotIndex+1,end);
-        }
-
-        return arr;
+            quickSortHelper(arr, pivotIndex,end);
+ 
     }
     
     /**
+     * In this algorithm we are going to move the pivot element to it's correct positon
      * Using the first element as pivot
      * keep track of a pointer, that we called pi(pivotIndex). 
      * Loop throught the array and if an element is smaller than the pivot, increment the pivot index, and swap the current element withthe element at pivot index.
@@ -52,6 +51,39 @@ public class QuickSort{
         swap(arr,start,pi);
         return pi;
     }
+
+    /**
+     * another approach to return the pivot index
+     * Here we select a random element to be the pivot, move the elements smaller than pivot to the left side of the pivot and elements greater to the right side of the pivot
+     * 
+     * @param arr
+     * @param start
+     * @param end
+     */
+    public int pivotHelper2(int[] arr, int start, int end){
+
+        int pivot = arr[(start+end)/2];
+
+        int left=start, right= end;
+
+        while(left<=right){
+            while(arr[left]<pivot){
+                left++;
+            }
+            while(arr[right]>pivot){
+                right--;
+            }
+            if(left<=right){
+                swap(arr,left,right);
+                left++;
+                right--;
+            }
+
+        }
+        return left;
+
+    }
+
     public void swap(int[] arr, int first,int second){
         int firstValue = arr[first];
         int secondValue = arr[second];
